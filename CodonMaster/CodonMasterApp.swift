e38f00fa38f4ca72435be6c64343f966dc398f7d
KYHSGeekCode <jourhyang123@gmail.com>
2022-07-19 16:29:58 +0900

    Initial Commit

---
+++ CodonMaster/CodonMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodonMasterApp.swift
+//  CodonMaster
+//
+//  Created by 양현서 on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct CodonMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
